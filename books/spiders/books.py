141e386d4be153966bd7fce29fbff9d62194b42c
guillermo <guillermo.pilleux@ing.uchile.cl>
2020-05-04 11:51:37 -0400

    test

--- books/spiders/books.py
+++ books/spiders/books.py
@@ -28,34 +28,34 @@ class BooksSpider(scrapy.Spider):
 
         articles = response.css(".Elemento")
         print(len(articles))
-        #e = articles[7]
-        for e in articles:
-            links = e.css("a::attr(href)").extract()
-            source = links[0]
-            print(source)
-            imgName = "".join(e.css("h2.Elemento__title").css("a::text").extract_first().split(" "))
+        e = articles[7]
+        #for e in articles:
+        links = e.css("a::attr(href)").extract()
+        source = links[2]
+        print(source)
+        imgName = "".join(e.css("h2.Elemento__title").css("a::text").extract_first().split(" "))
 
-            author = e.css(".Elemento__autor").css("a::text").extract_first()
-            #TODO
-            if author:
-                author = "".join(author.split(" "))
-            else:
-                author = "NoTieneAutor"
-            
-            imgLink = e.css("img::attr(src)").extract_first()
-            idSplit = imgLink.split("/")[-1].split(".")[0].split("-")
-            imgId = "-".join([idSplit[0], idSplit[1]])
-            finalImageName = "-".join([imgName, "por", author, colection, imgId])
-
-            #yield scrapy.Request(response.urljoin(colection_url), callback = self.download_img, meta={'imgLink': imgLink, 'finalImageName': finalImageName})
-        #yield scrapy.Request(response.urljoin(colection), callback=self.download_img, meta={'imgLink': imgLink, 'finalImageName': finalImageName})
+        author = e.css(".Elemento__autor").css("a::text").extract_first()
+        #TODO
+        if author:
+            author = "".join(author.split(" "))
+        else:
+            author = "NoTieneAutor"
         
-            item = ImageItem()
-            img_urls = []
-            img_urls.append(imgLink)
-            item["image_urls"] = img_urls
-            item["image_name"] = finalImageName
-            yield item
+        imgLink = e.css("img::attr(src)").extract_first()
+        idSplit = imgLink.split("/")[-1].split(".")[0].split("-")
+        imgId = "-".join([idSplit[0], idSplit[1]]).split("x")[1]
+        finalImageName = "-".join([imgName, "por", author, colection, imgId])
+
+        #yield scrapy.Request(response.urljoin(colection_url), callback = self.download_img, meta={'imgLink': imgLink, 'finalImageName': finalImageName})
+    #yield scrapy.Request(response.urljoin(colection), callback=self.download_img, meta={'imgLink': imgLink, 'finalImageName': finalImageName})
+    
+        item = ImageItem()
+        img_urls = []
+        img_urls.append(imgLink)
+        item["image_urls"] = img_urls
+        item["image_name"] = finalImageName
+        yield item
         
     
     def download_img(self, response):
