d4104da98ffb08490378ac1878a93d1186a01ca4
guillermo <guillermo.pilleux@ing.uchile.cl>
2020-05-03 23:42:37 -0400

    no author

--- books/spiders/books.py
+++ books/spiders/books.py
@@ -12,16 +12,29 @@ class BooksSpider(scrapy.Spider):
     start_urls = ['http://culturadigital.udp.cl/index.php/coleccion/fondo-sills-y-gallardo/']
 
     def parse(self, response):
+        #Get colections
+        #iterate over colections
+        #for colection_url in response.css("article.product_pod > h3 > a ::attr(href)").extract():
+        colection_name = "fondo-sills-y-gallardo"
+        colection_url = "http://culturadigital.udp.cl/index.php/coleccion/fondo-sills-y-gallardo"
+        yield scrapy.Request(response.urljoin(colection_url), callback = self.parse_colection meta={'colection': colection_name})
+
+    def parse_colection(self, response):
         item = ImageItem()
         img_urls = []
 
-        colection = "fondo-sills-y-gallardo"
+        colection = response.meta.get('colection')
         articles = response.css(".Elemento")
-        e = articles[0]
+        e = articles[2]
         links = e.css("a::attr(href)").extract()
         source = links[0]
         imgName = "".join(e.css("h2.Elemento__title").css("a::text").extract_first())
+
+        #TODO falta caso cuando no existe author
+
         author = "".join(e.css(".Elemento__autor").css("a::text").extract_first().split(" "))
+        #TODO
+        if not author: author = "NoTieneAutor"
         imgLink = e.css("img::attr(src)").extract_first()
         idSplit = imgLink.split("/")[-1].split(".")[0].split("-")
         imgId = "-".join([idSplit[0], idSplit[1]])
@@ -32,11 +45,7 @@ class BooksSpider(scrapy.Spider):
         item["image_name"] = finalImageName 
 
         return item
-        #for book_url in response.css("article.product_pod > h3 > a ::attr(href)").extract():
-        #    yield scrapy.Request(response.urljoin(book_url), callback=self.parse_book_page)
-        #next_page = response.css("li.next > a ::attr(href)").extract_first()
-        #if next_page:
-        #    yield scrapy.Request(response.urljoin(next_page), callback=self.parse)
+        
 
     def parse_book_page(self, response):
         item = {}
@@ -50,3 +59,10 @@ class BooksSpider(scrapy.Spider):
         ).extract_first()
         item['price'] = response.css('p.price_color ::text').extract_first()
         yield item
+
+
+'''
+next_page = response.css("li.next > a ::attr(href)").extract_first()
+if next_page:
+    yield scrapy.Request(response.urljoin(next_page), callback=self.parse)
+'''
